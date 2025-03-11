b3d93cad029df9f7895287a53d22782abdc70d2c
developergovindsrivastav <govindsrivastav800@gmail.com.com>
2025-03-11 19:25:32 +0530

    dtring

---
+++ string/method.py
@@ -0,0 +1,21 @@
+screen = "loading screen unloading screen"
+print(screen.upper())
+print(screen.lower())
+print(screen.strip())
+print(screen.replace("loading", "unloading"))
+# string to list
+print(screen.split(" "))
+print(screen.find("loading"))
+print(screen.count("loading"))
+quantity = 1
+print(f"the quantity of loading screen is {quantity}")
+
+# list to string
+list2 = ["bs","b", "c"]
+print(" ".join(list2))
+a_sentence = "Mohan said to sohan,"" he is a good person"
+print(a_sentence)
+a_sentence2 = "mohan said to sohan, \"he is a good person\""
+print(a_sentence2)
+path = r"c:\users\mohan\desktop"
+print(path)
---
+++ string/slicing.py
@@ -0,0 +1,3 @@
+chai = "game of thrones"
+slice_chai = chai[0:5:2]
+print(slice_chai)
